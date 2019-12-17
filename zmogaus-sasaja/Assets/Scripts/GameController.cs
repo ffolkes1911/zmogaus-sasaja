326c29386ff9437e40c8c1e52d686f086cae7c65
ffolkes1911 <36667727+ffolkes1911@users.noreply.github.com>
2019-12-17 19:25:00 +0200

    nodeLine can now find distance to line, added more circles and lines, GameController now changes 'playing' variable state on space KeyUp/KeyDown

--- zmogaus-sasaja/Assets/Scripts/GameController.cs
+++ zmogaus-sasaja/Assets/Scripts/GameController.cs
@@ -9,6 +9,9 @@ public class GameController : MonoBehaviour {
     Transform levelObjects;
     GameObject nodePrefab;
     GameObject linePrefab;
+    KeyCode playKey = KeyCode.Space;
+    bool playing = false;
+    bool gotNode = false;
 
     [HideInInspector] public Node startedNode; // node which you connect to another node
 
@@ -27,15 +30,69 @@ public class GameController : MonoBehaviour {
         // do level loading
 
         // initial testing code
+        float depth = levelObjects.transform.position.z;
         GameObject node1 = GameObject.Instantiate(nodePrefab, levelObjects) as GameObject;
-        node1.transform.localPosition = new Vector3(-1.249f, 0.352f, 0);
+        node1.name = "node1";
+        node1.transform.position = new Vector3(-1, 2, depth);
+        nodes.Add(node1.GetComponent<Node>());
 
         GameObject node2 = GameObject.Instantiate(nodePrefab, levelObjects) as GameObject;
-        node2.transform.localPosition = new Vector3(-0.133f, 0.668f, 0);
+        node2.name = "node2";
+        node2.transform.position = new Vector3(0.458f, 1.554f, depth);
+        nodes.Add(node2.GetComponent<Node>());
+
+        GameObject node3 = GameObject.Instantiate(nodePrefab, levelObjects) as GameObject;
+        node3.name = "node3";
+        node3.transform.position = new Vector3(-1.679f, 0.736f, depth);
+        nodes.Add(node3.GetComponent<Node>());
+
+        GameObject node4 = GameObject.Instantiate(nodePrefab, levelObjects) as GameObject;
+        node4.name = "node4";
+        node4.transform.position = new Vector3(-0.294f, 1.032f, depth);
+        nodes.Add(node4.GetComponent<Node>());
+
+        GameObject node5 = GameObject.Instantiate(nodePrefab, levelObjects) as GameObject;
+        node5.name = "node5";
+        node5.transform.position = new Vector3(1.306f, 0.792f, depth);
+        nodes.Add(node5.GetComponent<Node>());
+
 
         GameObject line1 = GameObject.Instantiate(linePrefab, levelObjects) as GameObject;
+        line1.name = "line1";
         NodeLine nodeline1 = line1.GetComponent<NodeLine>();
         nodeline1.Initialize(0.03f, node1.GetComponent<Node>(), node2.GetComponent<Node>());
+        lines.Add(nodeline1);
+
+        GameObject line2 = GameObject.Instantiate(linePrefab, levelObjects) as GameObject;
+        line2.name = "line2";
+        NodeLine nodeline2 = line2.GetComponent<NodeLine>();
+        nodeline2.Initialize(0.03f, node1.GetComponent<Node>(), node3.GetComponent<Node>());
+        lines.Add(nodeline2);
+
+        GameObject line3 = GameObject.Instantiate(linePrefab, levelObjects) as GameObject;
+        line3.name = "line3";
+        NodeLine nodeline3 = line3.GetComponent<NodeLine>();
+        nodeline3.Initialize(0.03f, node1.GetComponent<Node>(), node4.GetComponent<Node>());
+        lines.Add(nodeline3);
+
+        GameObject line4 = GameObject.Instantiate(linePrefab, levelObjects) as GameObject;
+        line4.name = "line4";
+        NodeLine nodeline4 = line4.GetComponent<NodeLine>();
+        nodeline4.Initialize(0.03f, node3.GetComponent<Node>(), node4.GetComponent<Node>());
+        lines.Add(nodeline4);
+
+        GameObject line5 = GameObject.Instantiate(linePrefab, levelObjects) as GameObject;
+        line5.name = "line5";
+        NodeLine nodeline5 = line5.GetComponent<NodeLine>();
+        nodeline5.Initialize(0.03f, node5.GetComponent<Node>(), node3.GetComponent<Node>());
+        lines.Add(nodeline5);
+
+        GameObject line6 = GameObject.Instantiate(linePrefab, levelObjects) as GameObject;
+        line6.name = "line6";
+        NodeLine nodeline6 = line6.GetComponent<NodeLine>();
+        nodeline6.Initialize(0.03f, node5.GetComponent<Node>(), node4.GetComponent<Node>());
+        lines.Add(nodeline6);
+
         // initial testing code
     }
 
@@ -50,7 +107,11 @@ public class GameController : MonoBehaviour {
 	
     void SetStartNode(Node node)
     {
-        startedNode = node;
+        if (gotNode)
+        {
+            startedNode = node;
+            gotNode = true;
+        }
     }
 
     bool CheckConnection(NodeLine line, Node node)
@@ -60,6 +121,25 @@ public class GameController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if (Input.GetKeyDown(playKey))
+        {
+            SetPlayingState(true);
+        }
+        else if(Input.GetKeyUp(playKey))
+        {
+            SetPlayingState(false);
+        }
+    }
+
+    void SetPlayingState(bool state)
+    {
+        if (state) // if enable playing state
+        {
+            playing = state;
+        }
+        else
+        {
+            playing = state;
+        }
+    }
 }
--- zmogaus-sasaja/Assets/Scripts/NodeLine.cs
+++ zmogaus-sasaja/Assets/Scripts/NodeLine.cs
@@ -10,8 +10,11 @@ public class NodeLine : MonoBehaviour {
     private LineRenderer line;
     private float lineWidth;
     private float colliderWidthMult = 6f;
+    private float circleColliderRadius = 0.2f;
 
     private float colliderDepth = 0.05f;
+
+    private float x, y, a, b;
 
     private bool used;
 
@@ -25,6 +28,7 @@ public class NodeLine : MonoBehaviour {
         used = false;
         SetLineWidth(width);
         Connect(start, end);
+        CalculateLineEquation(this.start.position, this.end.position);
         AddColliderToLine();
     }
 
@@ -40,17 +44,42 @@ public class NodeLine : MonoBehaviour {
         this.nodeEnd = end;
         this.start = nodeStart.transform;
         this.end = nodeEnd.transform;
-    }
-
-    public bool isConnected(Node node)
-    {
-        return Object.ReferenceEquals(node, start) || Object.ReferenceEquals(node, end);
-    }
-	
-	// Update is called once per frame
-	void Update () {
         line.SetPosition(0, start.transform.position);
         line.SetPosition(1, end.transform.position);
+    }
+
+    // line equation y = ax + b
+    public void CalculateLineEquation(Vector3 start, Vector3 end)
+    {
+        a = (end.y - start.y) / (end.x - start.x);
+        b = start.y - (a * start.x);
+        //Debug.Log(gameObject.name + " a=" + a + " b=" + b + "startpoint=[" + start.x + ":" + (a*start.x + b) + "]" + "endpoint=[" + end.x + ":" + (a * end.x + b) + "]");
+    }
+
+    public float GetPointY(float x)
+    {
+        return a * x + b;
+    }
+    public float GetPointX(float y)
+    {
+        return (y - b) / a;
+    }
+
+    public Vector3 GetClosestPoint(Vector3 P)
+    {
+        return Vector3.zero;
+    }
+
+    // can possibly use http://wiki.unity3d.com/index.php/3d_Math_functions
+    //this function assumes an infinite line
+    public float GetDistanceToPoint(Vector3 P)
+    {
+        Vector3 p1 = start.position;
+        Vector3 p2 = end.position;
+        float result = Mathf.Abs((p2.y - p1.y) * P.x - (p2.x - p1.x) * P.y + p2.x * p1.y - p2.y * p1.x) /
+            (Mathf.Sqrt((Mathf.Pow(p2.y - p1.y, 2) + Mathf.Pow(p2.x - p1.x, 2))));
+
+        return result;
     }
 
     // from https://answers.unity.com/questions/470943/collider-for-line-renderer.html
@@ -68,7 +97,7 @@ public class NodeLine : MonoBehaviour {
         float lineLength = Vector3.Distance(startPoint, endPoint);
         // size of collider is set where X is length of line, Y is width of line
         //z will be how far the collider reaches to the sky
-        lineCollider.size = new Vector3(colliderDepth, lineWidth * colliderWidthMult, lineLength);
+        lineCollider.size = new Vector3(colliderDepth, lineWidth * colliderWidthMult, lineLength - (circleColliderRadius * 2));
         // get the midPoint
         Vector3 midPoint = (startPoint + endPoint) / 2;
         // move the created collider to the midPoint
@@ -76,5 +105,17 @@ public class NodeLine : MonoBehaviour {
 
         // rotate the collider transform to look at the end node
         lineCollider.transform.LookAt(end.transform);
+    }
+
+    public bool isConnected(Node node)
+    {
+        return Object.ReferenceEquals(node, start) || Object.ReferenceEquals(node, end);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Transform pointer = GameObject.Find("testingPoint").transform;
+        //float dist = GetDistanceToPoint(pointer.position);
+        //Debug.Log("length: " + dist);
     }
 }
