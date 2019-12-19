b5ec34ff1fe9df79d62699bc5e7d5ae66a6e21f8
Butkus Eimantas <eimbut@ktu.lt>
2019-12-19 12:51:06 +0200

    Add Line creation in Level Creation

---
+++ zmogaus-sasaja/Assets/Scripts/LevelCreation/CreationNode.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreationNode : MonoBehaviour {
+    [SerializeField] private CreationController creationController;
+
+	// Use this for initialization
+	void Start ()
+    {
+        creationController = this.transform.parent.GetComponent<CreationController>();
+	}
+
+    private void OnMouseDown()
+    {
+        creationController.JoinNode(this.gameObject);
+    }
+}
